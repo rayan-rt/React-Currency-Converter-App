0d829a3a457f247d9503af05e497e17952cf9f75
Rayan <tanzeemrayan2@gmail.com>
2024-05-22 23:18:38 +0500

    writing axios code

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,14 +1,30 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
+
+// `https://api.currencyapi.com/v3/latest?apikey=cur_live_BFh3Z5NfWAsU9WCQctN6qg0rfpGG7YoFjGjG7Xvr&base_currency=${country_currency}`;
 
 function useCurrencyInfo(country_currency) {
 	let [data, setData] = useState({});
 
 	useEffect(() => {
-		fetch(
-			`https://api.currencyapi.com/v3/latest?apikey=cur_live_BFh3Z5NfWAsU9WCQctN6qg0rfpGG7YoFjGjG7Xvr&base_currency=${country_currency}`,
-		)
-			.then((response) => response.json())
-			.then((result) => setData(result.data));
+		const fetchCurrencyData = async () => {
+			try {
+				const response = await axios.get(
+					`https://api.currencyapi.com/v3/latest`,
+					{
+						params: {
+							apikey: "cur_live_BFh3Z5NfWAsU9WCQctN6qg0rfpGG7YoFjGjG7Xvr",
+							base_currency: country_currency,
+						},
+					},
+				);
+				setData(response.data.data);
+			} catch (error) {
+				console.error("Error fetching currency data:", error);
+			}
+		};
+
+		fetchCurrencyData();
 	}, [country_currency]);
 
 	return data;
